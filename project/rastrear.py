30cd6433a49a0b36d0b13d971495ddb6107064a1
Marcos Printes <67763353+MacosPrintes001@users.noreply.github.com>
2023-01-31 16:28:41 -0300

    teste

--- project/rastrear.py
+++ project/rastrear.py
@@ -2,12 +2,11 @@ import cv2
 import sys
 from random import randint
 
-cap = cv2.VideoCapture("assets/video.mp4") #pega o video
-
+cap = cv2.VideoCapture("assets/futebol.mp4") #pega o video
 
 ok, frame = cap.read()#verifica se o video foi lido corretamente
 if not ok:
-    print("Não foi possível ler o arquivo")
+    print("Cannot open file")
     sys.exit(1)
 
 caixas = []
@@ -17,7 +16,6 @@ while True:#recebe primeiro frame do video para escolher objeto de rastreio
     caixa = cv2.selectROI('Tracker', frame)
     caixas.append(caixa)
     imagens.append((randint(0,255),randint(0,255),randint(0,255)))
-    print("Pressione Q para sair ou qualquer outra para continuar proximmo objeto")
     if cv2.waitKey(0) == ord('q'):
         break
 
@@ -35,34 +33,11 @@ while cap.isOpened():
 
     ok , boxes = multitracker.update(frame)
 
-    for i, newbox in enumerate(boxes):##desenha a caixa nos frames que correspondem a imagem selecioanda inicialmente
+    for i, newbox in enumerate(boxes):#desenha a caixa nos frames que correspondem a imagem selecioanda inicialmente
         (x,y,w,h) = [int(v) for v in newbox]
-        cv2.rectangle(frame, (x,y), (x+w, y+h), imagens[i], 1,1)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), imagens[i], 2,2)
 
     cv2.imshow('MultiTracker', frame)
 
     if cv2.waitKey(1) == ord('e'):
         break
-
-
-
-# cap = cv.VideoCapture("assets/futebol.mp4")
-# if not cap.isOpened():
-#     print("Cannot open camera")
-#     exit()
-# while True:
-#     # Capture frame-by-frame
-#     ret, frame = cap.read()
-#     # if frame is read correctly ret is True
-#     if not ret:
-#         print("Can't receive frame (stream end?). Exiting ...")
-#         break
-#     # Our operations on the frame come here
-#     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-#     # Display the resulting frame
-#     cv.imshow('frame', gray)
-#     if cv.waitKey(1) == ord('q'):
-#         break
-# # When everything done, release the capture
-# cap.release()
-# cv.destroyAllWindows()
